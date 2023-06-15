548ab6eff2c6357cf65e116d1ed27ddd1ff981dc
kaylinkhanal10 <kyalinnop@gmail.com>
2023-06-15 10:22:30 +0545

    Added persist wrapper to app component

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,10 +1,14 @@
 import '@/styles/globals.css'
-import {Provider} from "react-redux";
-import store from "../redux/store";
+import { Provider } from "react-redux";
+import { persistor, store } from "../redux/store";
+import { PersistGate } from 'redux-persist/integration/react';;
+
 export default function App({ Component, pageProps }) {
   return (
-  <Provider store={store}>
-  <Component {...pageProps} />
-  </Provider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
   )
 }
